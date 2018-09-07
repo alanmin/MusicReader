d8600b2dec48371e34c68b192c1c6bb382f81c9d
Alan <min29@purdue.edu>
2018-09-07 12:55:13 -0400

    minor updates to dirichlet test

--- dirichlet_test.py
+++ dirichlet_test.py
@@ -1,13 +1,34 @@
 #Just testing out the dirichlet process clustering
 
 import numpy as np
+import scipy as sp
 import sklearn
 import matplotlib.pyplot as plt
+
+#Function that determines euclidean distance between two points.
+def d(x,y) :
+	if(len(x) != len(y)) :
+		raise ValueError('In d(x,y): len(x) != len(y)')
+	return(np.sqrt(np.sum(np.square(np.subtract(x,y)))))
+
+
+
+#TODO
+#Metropolis Hastings Sampler
+def metropolis_hastings() :
+	return None
+
+
+
+#TODO
+#Gibbs Sampler
+def gibbs_sampler() :
+	return None
+
 
 
 #Initialize three clusters: normals sd 1 centered at 0,0 and 5,5, and 10,10
 #10 points each
-
 zero = zip([np.random.normal() for i in range(10)],
 	[np.random.normal() for j in range(10)])
 zero = [z for z in zero]
@@ -22,9 +43,26 @@ ten = [t for t in ten]
 
 data = zero + five + ten
 
+
+
+#Initialize cluster assignments with 30 clusters
+#c is a 30 x K matrix, where each row is the format [0, ... , 1, 0, ...., 0]
+#	and the row with the 1 is the cluster that the observation belongs to
+c = np.identity(30, int)
+
+#Modeling with 0 covariance multivariate normal distributions. 
+#Initialize mean to be data
+theta = data
+
+
+
+
+
 data = np.array(data)
 print(data)
 
 p = plt.scatter(data[:,0], data[:,1])
 plt.show()
+
+
 
