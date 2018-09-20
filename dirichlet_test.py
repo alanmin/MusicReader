9035a207ac6b7701e65996e170b342934dad2729
Alan <min29@purdue.edu>
2018-09-19 22:24:59 -0700

    Updated dirichlet_test

--- dirichlet_test.py
+++ dirichlet_test.py
@@ -4,6 +4,10 @@ import numpy as np
 import scipy as sp
 import sklearn
 import matplotlib.pyplot as plt
+import random
+from scipy.stats import multivariate_normal
+
+ALPHA = 1
 
 #Function that determines euclidean distance between two points.
 def d(x,y) :
@@ -15,16 +19,34 @@ def d(x,y) :
 
 #TODO
 #Metropolis Hastings Sampler
-def metropolis_hastings() :
-	return None
+#x is the x value of the cluster we're trying to assign. Should be a list of lists
+	#since this should work for multidimensional data
+#xmi stands for x minus i. List of the x values of data points that aren't x
+#k is the cluster assignments for each of the xmi. Should be an atomic vector
+def assignment(x, xmi, k) :
+	unique_clusters = set(list(k.elements()))
 
+	#pick a random uniform r
+	r = random.uniform(0,1)
 
+	#This is a running total of the probabilities
+	s = 0
 
-#TODO
-#Gibbs Sampler
-def gibbs_sampler() :
-	return None
+	#converts the xmi and k into np arrays
+	xminp = np.array(xmi)
+	knp = np.array(k)
+	for c in unique_clusters : 
+		#This gives the elements in group c
+		w = np.where(knp == c)
 
+		#This unique_clusters[c] should give us n_k
+		if(r < unique_clusters[c]/(len(k) + ALPHA) * 
+			multivariate_normal(mean = xminp[w].mean(axis = 0)).pdf(x) ) :
+			return(c)
+	
+	#if not an existing cluster, return a new cluster
+	return(c)
+		
 
 
 #Initialize three clusters: normals sd 1 centered at 0,0 and 5,5, and 10,10
@@ -61,7 +83,8 @@ theta = data
 data = np.array(data)
 print(data)
 
-p = plt.scatter(data[:,0], data[:,1])
+
+p = plt.scatter(data[:,0], data[:,1], c = ["red"] * 30)
 plt.show()
 
 
